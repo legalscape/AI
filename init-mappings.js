d1413121927785c467d8d622d8bdbbc128759712
Yusuke Kido <yusuke.kido@legalscape.co.jp>
2018-05-24 14:10:43 +0000

    Now init-mappings is now more clever

--- init-mappings.js
+++ init-mappings.js
@@ -10,24 +10,36 @@ const myFilter = isPrivate => function({id, name}) {
 };
 
 (async _ => {
-  console.log('Fetching Asana user list...');
-  let users = {};
-  for (const {id, name} of (await asana.users.findByWorkspace(config.asana.organizationId)).data) {
-    users[name] = {asanaId: id};
+  const existing = jsonfile.readFileSync('mappings.json', {throws: false});
+
+  if (existing) {
+    console.log('mappings.json already exists and the existing mappings recorded in it will be kept.');
   }
 
-  console.log('Fetching Slack user list...');
-  for (const {id, name, real_name} of (await slack.users.list()).members) {
-    if (users[real_name]) {
-      users[real_name].slackId = id;
-    } else if (users[name]) {
-      users[name].slackId = id;
-    } else {
-      users[real_name] = {slackId: id};
+  console.log('Fetching Asana user list...');
+  let users = {};
+  const knownAsanaUsers = existing ? existing.users.map(o => o.asana) : [];
+  for (const {id, name} of (await asana.users.findByWorkspace(config.asana.organizationId)).data) {
+    if (knownAsanaUsers.indexOf(id) < 0) {
+      users[name] = {asanaId: id};
     }
   }
 
-  let found = [], error = [];
+  console.log('Fetching Slack user list...');
+  const knownSlackUsers = existing ? existing.users.map(o => o.slack) : [];
+  for (const {id, name, real_name} of (await slack.users.list()).members) {
+    if (knownSlackUsers.indexOf(id) < 0) {
+      if (users[real_name]) {
+        users[real_name].slackId = id;
+      } else if (users[name]) {
+        users[name].slackId = id;
+      } else {
+        users[real_name] = {slackId: id};
+      }
+    }
+  }
+
+  let found = existing ? existing.users : [], error = [];
   for (const name in users) {
     const obj = {
       name,
@@ -41,15 +53,24 @@ const myFilter = isPrivate => function({id, name}) {
     }
   }
 
-  console.log(`Built user mapping (found: ${found.length}, error: ${error.length}).`);
+  if (existing) {
+    console.log(`Built user mapping (existing: ${existing.users.length}, newly found: ${found.length - existing.users.length}, error: ${error.length}).`);
+  } else {
+    console.log(`Built user mapping (found: ${found.length}, error: ${error.length}).`);
+  }
 
   console.log('Fetching Asana projects list...');
-  let teams = (await asana.teams.findByOrganization(config.asana.organizationId)).data;
-  for (let i = 0; i < teams.length; ++i) {
-    teams[i].projects = (await asana.projects.findByTeam(teams[i].id, {archived: false})).data;
+  const knownAsanaProjects = existing ? existing.channels.map(o => o.asana) : [];
+  let asanaTeams = (await asana.teams.findByOrganization(config.asana.organizationId)).data;
+  for (let i = 0; i < asanaTeams.length; ++i) {
+    asanaTeams[i].projects =
+      (await asana.projects.findByTeam(asanaTeams[i].id, {archived: false}))
+      .data
+      .filter(p => knownAsanaProjects.indexOf(p.id) < 0);
   }
 
   console.log('Fetching Slack channel list...');
+  const knownSlackChannels = existing ? existing.channels.map(o => o.slack) : [];
   const res1 = await slack.channels.list({
     exclude_archived: true,
     limit: 100,
@@ -61,10 +82,15 @@ const myFilter = isPrivate => function({id, name}) {
     exclude_archived: true,
   });
 
+  const slackChannels = res1.channels.map(myFilter(false))
+        .concat(res2.groups.map(myFilter(true)))
+        .filter(c => knownSlackChannels.indexOf(c.id) < 0)
+        .sort((x, y) => x.name.localeCompare(y.name));
+
   console.log('Saving to mappings.json...');
   jsonfile.writeFileSync('mappings.json', {
     users: found,
-    channels: [
+    channels: existing ? existing.channels : [
       {
         name: 'example',
         asana: 1234567890,
@@ -74,11 +100,11 @@ const myFilter = isPrivate => function({id, name}) {
     hint: {
       users: error,
       channels: {
-        asana: teams,
-        slack: res1.channels.map(myFilter(false))
-          .concat(res2.groups.map(myFilter(true)))
-          .sort((x, y) => x.name > y.name),
+        asana: asanaTeams,
+        slack: slackChannels,
       }
     }
   }, {spaces: 2});
+
+  console.log('Now please edit mappings.json manually.');
 })();
