c1d744071c68c52d147c36cfd179f3521e05c156
Yusuke Kido <yusuke.kido@legalscape.co.jp>
2020-03-23 02:08:07 +0000

    期限切れのタスクをやらない人がいる

--- outdated.js
+++ outdated.js
@@ -9,7 +9,7 @@ const slack = new WebClient(config.slack.botUserAccessToken);
 (async () => {
     const today = dateism('YYYY-MM-DD').today();
     const channel = config.slack.announcementChannelId;
-    const text = 'These people has nothing on their outdated task list :sparkles: Super :sunglasses:\n' +
+    const text = 'Asanaに登録されている今日中に期限切れになるタスクの件数\n' +
         (await Promise.all(getUsers().map(async user => ({
             user,
             tasks: await asana.tasks.searchInWorkspace(config.asana.organizationId, {
@@ -18,9 +18,10 @@ const slack = new WebClient(config.slack.botUserAccessToken);
                 'due_on.before': today,
             }),
         }))))
-            .filter(({ tasks }) => tasks.data.length === 0)
-            .map(({ user }) => `<@${user.slack}>`)
-            .join(' ') + '\nAll others have some tasks behind schedule... :sad:';
+            .filter(({ tasks }) => tasks.data.length > 0)
+            .sort((a, b) => b.tasks.data.length - a.tasks.data.length)
+            .map(({ user, tasks }) => `<@${user.slack}>: ${tasks.data.length} 件\n　${tasks.data.map(task => '<https://app.asana.com/0/0/' + task.gid + '|' + task.name + '>').join(', ')}`)
+            .join('\n') + '\n適切な期限を再設定しましょう！';
 
     await slack.chat.postMessage({ channel, text });
 })();
